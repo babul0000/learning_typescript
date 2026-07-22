082972960c869324834e94f84ee19ee391e3cb1d
Your Name <babulhossan.info@gmail.com>
2026-07-22 22:40:33 +0600

    array maps

--- src/arrayMap.ts
+++ src/arrayMap.ts
@@ -1,8 +1,6 @@
 const number: number[] = [1, 2, 3, 4, 5];
 
 const double = number.map(nu => nu-2);
-// console.log(double);
-const 
-
+console.log(double);
 const filter = number.filter(nu => nu < 2 );
 console.log(filter);
